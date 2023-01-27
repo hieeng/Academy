14261082029ac8be386d0f81bc9afa64f4788155
Nam WonHee <q489q489@gmail.com>
2023-01-27 13:48:24 +0900

    commit

---
+++ Assets/Script2/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public Player player;
+    public MainCamera mainCamera;
+    public UIManager uIManager;
+    public GameObject enemys;
+    public int EnemyCount;
+    public int EnemyKillCount;
+
+    private void Awake()
+    {
+        instance = this;
+        EnemyCount = enemys.transform.childCount;
+    }
+}
